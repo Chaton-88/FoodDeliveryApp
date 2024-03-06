d4f6521bb0f25d2d053cc6ed86231092fbee1dd3
Valeriya Trofimova <chaton-2410@mail.ru>
2024-03-07 00:10:40 +0300

    Added new base colors

---
+++ FoodDeliveryApp/FoodDeliveryApp/Resources/AppColors.swift
@@ -0,0 +1,19 @@
+//
+//  AppColors.swift
+//  FoodDeliveryApp
+//
+//  Created by Valeriya Trofimova on 06.03.2024.
+//
+
+import UIKit
+
+enum AppColors {
+    static let accentPink = UIColor().hex(0xFB7A7A)
+    static let litePink = UIColor().hex(0xFFCACA)
+    static let superLitePink = UIColor().hex(0xE5E5E5)
+    static let tabBarIconColor = UIColor().hex(0xEB7070)
+    static let black = UIColor().hex(0x000000)
+    static let white = UIColor().hex(0xFFFFFF)
+    static let gray = UIColor().hex(0xC4C4C4)
+    static let bright = UIColor().hex(0xEC008C)
+}
